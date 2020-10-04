6ca8eb3d8dce322d36e71290e728f662993a4345
Grigor Aleksanyan <grigdevelop@gmail.com>
2020-10-04 02:35:00 +0200

    code cleanup

--- src/GZipTest/Program.cs
+++ src/GZipTest/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.IO.MemoryMappedFiles;
-using System.Text;
 using Veeam.Gzipper.Cmd.IO;
 using Veeam.Gzipper.Cmd.Logging;
 using Veeam.Gzipper.Core;
@@ -16,39 +13,8 @@ namespace GZipTest
 {
     class Program
     {
-
-        static void _Main(string[] args)
-        {
-            using var mmf = MemoryMappedFile.CreateFromFile("source.txt");
-            using (var view = mmf.CreateViewStream(0, 20))
-            {
-                ShowStream(view);
-            }
-
-            Console.WriteLine("-----------------------------");
-
-            using (var view = mmf.CreateViewStream(20, 20))
-            {
-                ShowStream(view);
-            }
-        }
-
-        static void ShowStream(Stream stream)
-        {
-            var buffer = new byte[3];
-            var read = stream.Read(buffer);
-            while (read > 0)
-            {
-                Console.WriteLine(Encoding.Default.GetString(buffer));
-                read = stream.Read(buffer);
-            }
-        }
-
         static void Main(string[] args)
         {
-            args = new[] { "compress", "source.txt", "target.zip" };
-            args = new[] { "decompress", "target.zip", "s.txt" };
-
             // dependencies
             IInputOutput io = new ConsoleInputOutput();
             ILogger logger = new ConsoleLogger();
